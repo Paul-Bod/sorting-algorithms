e2244e76f79dbbdd15912abca233358d2195f94d
Paul Ladyman <paul.ladyman@bbc.co.uk>
2014-05-16 16:00:24 +0100

    Bubblesort and InPlaceQuicksort now take a List, copy it to a Buffer for in place processing, and return the Buffer as a List. Also updated Analysis Object to run tests of each algorithm against an input list.

--- src/main/scala/Sort.scala
+++ src/main/scala/Sort.scala
@@ -1,7 +1,16 @@
+import scala.collection.mutable.Buffer
+
 trait Sort {
   def sort(list: List[Int]): List[Int]
 
   def apply(list: List[Int]): List[Int] = {
     sort(list)
+  }
+
+  def swap(leftIndex: Int, rightIndex: Int, list: Buffer[Int]) = {
+    val leftItem = list(leftIndex)
+    val rightItem = list(rightIndex)
+    list.update(leftIndex, rightItem)
+    list.update(rightIndex, leftItem)
   }
 }
