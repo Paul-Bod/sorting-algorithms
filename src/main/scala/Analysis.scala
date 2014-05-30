6b2f653d08e7c5c3d412d906e6e511890ae78b5f
Paul Ladyman <paul.ladyman@bbc.co.uk>
2014-05-30 14:51:42 +0100

    Added optional timeout parameter to Analysis runner.

--- src/main/scala/Analysis.scala
+++ src/main/scala/Analysis.scala
@@ -1,15 +1,20 @@
+import java.util.concurrent.TimeoutException
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+import ExecutionContext.Implicits.global
+
 object Analysis {
-  def run(list: List[Int]) {
+  def run(list: List[Int], timeout: Option[Int] = None) {
     println("Analysing list")
     println("============================")
-    new Thread ( new SortRunner("Bubblesort", list)).start
-    new Thread ( new SortRunner("Mergesort", list)).start
-    new Thread ( new SortRunner("SimpleQuicksort", list)).start
-    new Thread ( new SortRunner("InPlaceQuicksort", list)).start
+    new Thread ( new SortRunner("Bubblesort", list, timeout)).start
+    new Thread ( new SortRunner("Mergesort", list, timeout)).start
+    new Thread ( new SortRunner("SimpleQuicksort", list, timeout)).start
+    new Thread ( new SortRunner("InPlaceQuicksort", list, timeout)).start
   }
 }
 
-class SortRunner(label: String, list: List[Int]) extends Runnable {
+class SortRunner(label: String, list: List[Int], timeout: Option[Int]) extends Runnable {
   override def run() {
     val sorter = label match {
       case "Bubblesort" => Bubblesort
@@ -18,10 +23,23 @@ class SortRunner(label: String, list: List[Int]) extends Runnable {
       case "InPlaceQuicksort" => InPlaceQuicksort
       case _ => throw new Exception("Must specify a valid MutableSort algorithm")
     }
-    val start = System.currentTimeMillis()
-    sorter(list)
-    val stop = System.currentTimeMillis()
-    println(s"$label took ${stop - start}ms")
+
+    try {
+      val start = System.currentTimeMillis()
+
+      timeout match {
+        case Some(x) => Await.result(Future {
+          sorter(list)
+        }, Duration.create(x, "s"))
+        case None => sorter(list)
+      }
+
+      val stop = System.currentTimeMillis()
+      println(s"$label took ${stop - start}ms")
+    }
+    catch {
+      case e: TimeoutException => println(s"$label timed out after 10 seconds")
+    }
   }
 }
 
