7bd2d9d94632ddfad5a69b321f96186aa6ca0bc4
Paul Ladyman <paul.ladyman@bbc.co.uk>
2014-05-30 13:28:49 +0100

    Fixed Stackoverflow error in Mergesort by making the merge method tail recursive.

--- src/main/scala/Mergesort.scala
+++ src/main/scala/Mergesort.scala
@@ -1,40 +1,49 @@
+import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
 object Mergesort extends Sort {
   def sort(list: List[Int]): List[Int] = {
     if (list.size > 1) {
+      val result = ListBuffer.empty[Int]
       val (left, right) = list.splitAt(list.size / 2)
-      merge(sort(left), sort(right), ListBuffer.empty[Int])
+      merge(sort(left), sort(right), result)
+      result.toList
     }
     else list
   }
 
-  private def merge(left: List[Int], right: List[Int], destination: ListBuffer[Int]): List[Int] = {
+  @tailrec
+  private def merge(left: List[Int], right: List[Int], destination: ListBuffer[Int]): Unit = {
     val leftSize = left.size
     val rightSize = right.size
 
-    if (leftSize > 0 && rightSize > 0) {
-      val firstLeft = left.head
-      val firstRight = right.head
+    var newLeft = left
+    var newRight = right
 
-      if (firstLeft <= firstRight) {
-        destination += firstLeft
-        merge(left.tail, right, destination)
-      }
-      else {
-        destination += firstRight
-        merge(left, right.tail, destination)
-      }
-    }
-    else if (leftSize > 0) {
-      destination += left.head
-      merge(left.tail, right, destination)
-    }
-    else if (rightSize > 0) {
-      destination += right.head
-      merge(left, right.tail, destination)
-    }
+    if (leftSize > 0 || rightSize > 0) {
+      if (leftSize > 0 && rightSize > 0) {
+        val firstLeft = left.head
+        val firstRight = right.head
 
-    destination.toList
+        if (firstLeft <= firstRight) {
+          destination += firstLeft
+          newLeft = left.tail
+        }
+        else {
+          destination += firstRight
+          newRight = right.tail
+        }
+      }
+      else if (leftSize > 0) {
+        destination += left.head
+        newLeft = left.tail
+      }
+      else if (rightSize > 0) {
+        destination += right.head
+        newRight = right.tail
+      }
+
+      merge(newLeft, newRight, destination)
+    }
   }
 }
